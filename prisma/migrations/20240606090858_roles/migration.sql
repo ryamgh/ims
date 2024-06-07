d62e1188d7ee6f76b3e66e523e23e70b4a0edc94
Ram Ghimire <ramghimire@Rams-MacBook-Air.local>
2024-06-07 13:15:39 +0545

    project setup and role schema

---
+++ prisma/migrations/20240606090858_roles/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `roles` (
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(40) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `roles_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
